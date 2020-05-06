7eb5eb38307e1cf0c8aaf7ff1f7d0e55f67cc2a2
thiago-carv <tdecarv@gmail.com>
2020-05-06 16:36:52 -0400

    Initial commit

---
+++ src/test/java/caesars/cipher/CaesarsCipherTest.java
@@ -0,0 +1,26 @@
+package caesars.cipher;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CaesarsCipherTest {
+	
+	private CaesarsCipher caesarsCipherTest = new CaesarsCipher();
+
+	@Test
+	void testCipheredMessageWithOffsetOf12() {
+		assertEquals("tai mdq kag pauzs fapmk", caesarsCipherTest.cipher("how are you doing today", 12));
+	}
+	
+	@Test
+	void testEmptyString() {
+		assertEquals("", caesarsCipherTest.cipher("", 12));
+	}
+	
+//	@Test
+//	void testCipheredMessageWithOffsetOf11() {
+//		assertEquals("tai mdq kag pauzs fapmk", caesarsCipherTest.cipher("how are you doing", 11));
+//	}
+	
+}
